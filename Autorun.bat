e4ff8f3de19d72c7d088cfc34da2f349316a6d75
Osama Hassanein <player1os@gmail.com>
2018-06-27 16:44:34 +0200

    Simplify the autorun script

--- Autorun.bat
+++ Autorun.bat
@@ -1,14 +1,8 @@
 @ECHO OFF
 
-:: Switch to the deploy directory.
-PUSHD \\kedata\Data1\B2B_Business_Inteligence\Osama Hassanein\ExcelWorkbookSplitter
-
 :: Set the autorun parameters.
 SET APP_IS_AUTORUN_MODE=TRUE
 SET APP_WORKBOOK_FILE_PATH=H:\WORKBOOK.xlsx
 
 :: Run the main project workbook.
 CALL "WorkbookSplitter.xlsm"
-
-:: Return to the original working directory.
-POPD
